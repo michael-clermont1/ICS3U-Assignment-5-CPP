4c398e3f564c8ae903aa5992b03d6a5a3ba39fcf
Michael Clermont <michael.clermont@mths.ca>
2022-05-13 03:39:49 +0000

    edited integer program

--- integer.cpp
+++ integer.cpp
@@ -6,11 +6,11 @@
 
 #include <iostream>
 #include <string>
+#include <limits>
 
 int main() {
     // this function is to record integers until the user ends it
-    std::string userEnd;
-    int counter = 0;
+    char userEnd;
     int positiveCount = 0;
     int negativeCount = 0;
     int zeroCount = 0;
@@ -19,36 +19,13 @@ int main() {
     // process & output
     while (true) {
         try {
-            if (counter == 1) {
-                std::cout <<
-                "Do you want to end the program and get your results (Y/N): ";
-                std::cin >> userEnd;
-            } else if (counter == 0) {
-                std::cout << "Enter a number to record: ";
-                std::cin >> numbersEntered;
-            }
-            if (userEnd == "Y" || userEnd == "y") {
-                std::cout << "\nPositive numbers: "
-                << positiveCount << ", negative numbers: "
-                << negativeCount << ", zeros: " << zeroCount << std::endl;
-                break;
-            } else if (userEnd == "N" || userEnd == "n") {
-                std::cout << "Enter a number to record: ";
-                std::cin >> numbersEntered;
-            } else if (userEnd != "Y" || userEnd != "y"
-            || userEnd != "N" || userEnd != "n") {
-                counter = counter + 1;
-                if (numbersEntered > 0) {
-                    positiveCount = positiveCount + 1;
-                } else if (numbersEntered < 0) {
-                    negativeCount = negativeCount + 1;
-                } else if (numbersEntered == 0) {
-                    zeroCount = zeroCount + 1;
-                }
-                continue;
-            } else {
-                std::cout << "That is not yes or no." << std::endl;
-                break;
+            std::cout << "Enter a number to record: ";
+            std::cin >> numbersEntered;
+            if (std::cin.fail()) {
+                std::cin.clear();
+                std::cin.ignore(
+                std::numeric_limits<std::streamsize>::max(), '\n');
+                throw "error";
             }
             if (numbersEntered > 0) {
                 positiveCount = positiveCount + 1;
@@ -57,9 +34,24 @@ int main() {
             } else if (numbersEntered == 0) {
                 zeroCount = zeroCount + 1;
             }
-        } catch (std::invalid_argument) {
-            std::cout << "Invalid input." << std::endl;
-            break;
+            std::cout <<
+            "Do you want to end the program" <<
+            "and get your results (Y/N (default N)): ";
+            std::cin >> userEnd;
+            if (std::cin.fail()) {
+                std::cin.clear();
+                throw "error";
+            }
+            if (userEnd == 'Y' || userEnd == 'y') {
+                std::cout << "\nPositive numbers: "
+                << positiveCount << ", negative numbers: "
+                << negativeCount << ", zeros: " << zeroCount << std::endl;
+                break;
+            }
+        } catch (const char* error) {
+            std::cout << "\nInvalid input, try again." << std::endl;
+            numbersEntered = 0;
+            continue;
         }
     }
     std::cout << "\nDone." << std::endl;
